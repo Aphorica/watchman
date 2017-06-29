5427d0c3c310d1aa69819cc40890b47a8c655d45
rickb <rbmailer-contact@yahoo.com>
2017-06-29 16:16:28 -0600

    Allows single files to be specified in the sass section.
    Updated documentation.

--- README.md
+++ README.md
@@ -1,3 +1,79 @@
 # watchman
+### Rick Berger, Aphorica Inc (rickb@aphorica.com)
+---
+The Sass folks (http://sass-lang.com) have re-written the sass processor in
+Dart, which is an awesome thing.
 
-A sample command-line application.
+However, what isn't implemented (as of yet) is the 'watch' capability.
+
+Here is a little facility to provide that capability for Sass and also for
+file copies (which I needed.)
+
+It is driven by a yaml config file:
+
+    root: ( optional - uses the cwd, if not specified)
+    sass:
+      - lib
+      - web/styles.css
+    copy:
+      - src-path: README.md
+        dst-path: web/assets/about.md
+
+#### Notes:
+- As noted, the root is optional.
+- In the 'sass' section: 
+  - if the entry is a directory, the provided path will be
+    scanned for files ending in '.sass' or '.scss'.  Those files will be added to
+    the watch list.
+
+  - If the entry is a single file, it will be added to the watch list (assuming it
+    ends in '.sass' or '.scss').  This is one way to avoid other files you don't
+    want watched or subdirectories you don't want scanned.
+
+- On startup, the destination files are checked for existence.  If they are not, the
+  correct procedure is invoked to create them.
+
+- You can specify as many entries as you want.
+
+## Installing as executable
+Until I get it into the pub registry, do a git clone and:
+
+    pub global activate --source path _(path to cloned dir)_ --overwrite
+
+## Running
+- Create a configuration file.  If 'root' is not specified, it will use the 'cwd' as
+  the root path.  (If this is not correct, relative file specs will error.)
+
+- Run _watchman_, pointing to the config file:
+
+      watchman <-v> configfile.yaml <&>
+
+If you specify '-v', it will output information on what it thinks it is doing.  Probably
+not a bad thing to do the first couple of times you run it.
+
+(The ampersand ('&') is for *nix/osx users to put it in background mode.)
+
+## Caveats
+This is _very_ lightly tested.  I plan to revisit with a more comprehensive test suite,
+but it's working well enough for my purposes to use.  I'm putting it up on github so I
+can pull it down to my various build environments.
+
+I'm using it on OSX.  I wrote it to be FS agnostic, so it should work on Windows - certainly *nix -- but haven't tested it on those platforms, yet.
+
+If you have need for something, ping me.  I can probably add something reasonable quickly.
+
+## Future
+I'm thinking about adding a general 'cmd' section that would allow any arbitrary process
+that takes an input file and spits out an output file to be invoked.  Could be useful
+for things like minification, filters, whatever.
+
+The section would look something like:
+
+    ...
+    cmd:
+      - cmdstr: "minify -q -i %src-path% -o %dst-path%"
+        src-path: "js/tools.js"
+        dst-path: "jslibs/tools.js.min"
+    ...
+
+Or something like that.
--- bin/watchman.dart
+++ bin/watchman.dart
@@ -1,15 +1,16 @@
-// Copyright (c) 2017, rickb. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2017, rickb, Aphorica Inc. All rights reserved. Use of this source code
+// is governed by a MIT-style license that can be found in the LICENSE file.
 import 'dart:io';
-import 'dart:convert';
 
+import 'package:path/path.dart' as p;
+import 'package:yaml/yaml.dart';
 import 'package:watcher/watcher.dart';
 import 'package:sass/sass.dart' as Sass;
 
 main(List<String> args) {
   bool verbose = false;
-  String configFile = '', configString;;
-  Map<String, String> cmdList = <String,String>{};
+  String configFile = '', configString;
+  String rootPath = '.';
 
   for (int argIX = 0; argIX < args.length; argIX++) {
           // scan for flags, first
@@ -38,97 +39,132 @@ main(List<String> args) {
     configString = fi.readAsStringSync();
   }
 
-  Map<String,dynamic> cfg;
+  YamlMap cfg;
 
   try {
-    cfg = JSON.decode(configString);
+    cfg = loadYaml(configString);
   }
   catch(e) {
-    print ("ERROR: $e");
+    print ("ERROR: Reading config");
+    print ("$e");
     return 1;
+  }
+
+  if (cfg.containsKey('root')) {
+    rootPath = cfg['root'];
+  }
+
+  rootPath = p.canonicalize(rootPath);
+
+  if (verbose) {
+    print ('Root path: "$rootPath"');
   }
 
   if (cfg.containsKey('sass')) {
     if (verbose)
-      print ('Process sass files:');
+      print ('Processing \'sass\' section:');
     List<String> sassPaths = cfg['sass'];
     for (String path in sassPaths) {
-
-      if (verbose)
-        print('  Scanning $path...');
-
-      Directory dir = new Directory(path);
-      if (dir.existsSync()) {
-        for (FileSystemEntity entity in dir.listSync(recursive:true)) {
-          if (entity.statSync().type == FileSystemEntityType.FILE &&
-               entity.path.endsWith('.scss') || entity.path.endsWith('.sass')) {
-            String fullPath = (entity.absolute.path);
-            if (verbose)
-              print ('    Adding file: "${entity.path}"');
-              
-            FileWatcher watcher = new FileWatcher(fullPath);
-            watcher.events.listen((event) {
-              String sassStr = Sass.render(fullPath);
-              String outPath = fullPath.replaceFirst(new RegExp('\.s[ca]ss'), '.css');
-              File outFile = new File(outPath);
-              if (verbose)
-                print('Sass output: $fullPath => $outPath');
-
-              try {
-                outFile.writeAsStringSync(sassStr);
-              } catch (e) {
-                print("ERROR: writing to $outPath");
-                print(e);
-              }
-            });          
-          }
-        }
+      File sassFile = new File(path);
+      if (sassFile.statSync().type == FileSystemEntityType.FILE) {
+        processSassFile(sassFile, verbose);
       } else {
-        print('ERROR: Sass search path "$path" does not exist');
+        if (verbose)
+          print('  Scanning $path...');
+
+        Directory dir = new Directory(path);
+        if (dir.existsSync()) {
+          for (FileSystemEntity entity in dir.listSync(recursive:true)) {
+            processSassFile(entity, verbose);
+          }
+        } else {
+          print('ERROR: Sass search path "$path" does not exist');
+        }
       }
     }
   }
 
-/*
-  if (cfg.containsKey('cmd')) {
-    List<Map<String, String>> cmdEntities = cfg['cmd'];
+  if (cfg.containsKey('copy')) {
+    if (verbose)
+      print('Processing \'copy\' section:');
+    List<Map<String, String>> cmdEntities = cfg['copy'];
     for (Map<String, String> cmdEntity in cmdEntities) {
+      File fi = new File(cmdEntity['src-path']);
+      if (fi.existsSync() && fi.statSync().type == FileSystemEntityType.FILE) {
+        String fullSrcPath = fi.absolute.path;
+        String fullDstPath = cmdEntity['dst-path'];
+        if (!p.isAbsolute(fullDstPath))
+          fullDstPath = p.join(rootPath,fullDstPath);
 
-    }
-  }
+        if (verbose) {
+          print ("  Adding item:");
+          print ("    SrcPath: $fullSrcPath");
+          print ("    DstPath: $fullDstPath");
+        }
 
-  return 0;
+        File fiDst = new File(fullDstPath);
+        if (!fiDst.existsSync())
+          copyFile(fullSrcPath, fullDstPath, verbose);
 
-    String fn = p.absolute(arg);
+        FileWatcher watcher = new FileWatcher(fullSrcPath);
+        watcher.events.listen((event) {
+          copyFile(fullSrcPath, fullDstPath, verbose);
+        });          
 
-    File fi = new File(fn);
-    if (fi.existsSync() && fi.statSync().type == FileSystemEntityType.FILE) {
-      fnlist.add(fn);
-      if (verbose) {
-        print("Added: $fn");
+      }
+
+      else {
+        print ('  ERROR: file "${cmdEntity['path']}" does not exist or is not a file');
       }
     }
-
-    else if (verbose) {
-      print('Error: "$fn" is not a file - ignoring');
-    }
   }
-
-  if (fnlist.isNotEmpty) {
-    for (String fn in fnlist) {
-      print ('adding watcher on: "$fn"');
-      FileWatcher watcher = new FileWatcher(fn);
-      watcher.events.listen((event) {
-        print('Got an event...');
-        print(event);
-      });
-    }
-  }
-
-  else {
-    print("Usage: watch <-v> [file list]");
-  }
-  */
 
   return 0;
+}
+
+void processSassFile(FileSystemEntity entity, bool verbose) {
+  if (entity.statSync().type == FileSystemEntityType.FILE &&
+      entity.path.endsWith('.scss') || entity.path.endsWith('.sass')) {
+  String fullPath = (entity.absolute.path);
+  if (verbose)
+    print ('    Adding file: "${entity.path}"');
+    
+  String outPath = fullPath.replaceFirst(new RegExp('\.s[ca]ss'), '.css');
+  File outFile = new File(outPath);
+  if (!outFile.existsSync())
+    sassFile(fullPath, outPath, verbose);
+
+  FileWatcher watcher = new FileWatcher(fullPath);
+  watcher.events.listen((event) {
+      sassFile(fullPath, outPath, verbose);
+    });          
+  }
+}
+
+void copyFile(String src, String dst, bool verbose) {
+  File fi = new File(src);
+  if (verbose) {
+    print ('Copying: "$src" => "$dst"');
+  }
+  try {
+    fi.copySync(dst);
+  } catch (e) {
+    print('ERROR: copying $src to $dst');
+    print(e);
+  }
+}
+
+void sassFile(String src, String dst, bool verbose) {
+  String sassStr = Sass.render(src);
+  File outFile = new File(dst);
+  if (verbose) {
+    print('Sass output: "$src" => "$dst"');
+  }
+
+  try {
+    outFile.writeAsStringSync(sassStr);
+  } catch (e) {
+    print("ERROR: writing to $dst");
+    print(e);
+  }
 }
